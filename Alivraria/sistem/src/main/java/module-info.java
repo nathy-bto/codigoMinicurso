1e8577bbbcd7af3811be559495eae6ecea8d7f73
Leandro Souza <leandro.costa@ifba.edu.br>
2024-03-07 19:46:50 -0300

    movido arquivos fxml para pasta correta, ajuste no module-info e pacote do Controller no FXML

--- Alivraria/sistem/src/main/java/module-info.java
+++ Alivraria/sistem/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module br.edu.ifba.saj.ads.poo {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens br.edu.ifba.saj.ads.poo to javafx.fxml;
+    opens br.edu.ifba.saj.ads.poo.controle to javafx.fxml;
     exports br.edu.ifba.saj.ads.poo.classes;
     exports br.edu.ifba.saj.ads.poo.controle;
     exports br.edu.ifba.saj.ads.poo.exceptions;
